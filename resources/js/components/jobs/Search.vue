12024dd5b8b8a7ccad068c6847d3bc80bffcd0c1
Jun Santos <xjunsantos@gmail.com>
2022-07-30 01:33:39 +0800

    loading container

--- resources/js/components/jobs/Search.vue
+++ resources/js/components/jobs/Search.vue
@@ -69,7 +69,20 @@
 
                     <div class="col-lg-6">
                         <div class="row">
-                            <div v-for="job in jobs" :key="job.id" class="col-md-6">
+                            <div v-if="show_job_container" class="col-md-12">
+                                <content-loader
+                                    viewBox="0 0 540 740"
+                                    :speed="2"
+                                    primaryColor="#e1dbdb"
+                                    secondaryColor="#ecebeb"
+                                >
+                                    <rect x="12" y="20" rx="0" ry="0" width="250" height="250" /> 
+                                    <rect x="300" y="20" rx="0" ry="0" width="250" height="250" /> 
+                                    <rect x="12" y="322" rx="0" ry="0" width="250" height="250" /> 
+                                    <rect x="300" y="322" rx="0" ry="0" width="250" height="250" />
+                                </content-loader>
+                            </div>
+                            <div v-else v-for="job in jobs" :key="job.id" class="col-md-6">
                                 <div class="trainer-item">
                                     <div class="down-content">
                                         <img src="https://www.gulftalent.com/gtimages/large/SE350-10405_logo-img-cloud-spaces-careers-jobs.jpg" >
@@ -84,12 +97,29 @@
                                         </ul>
                                     </div>
                                 </div>
-                            </div>
+                            </div>                          
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <div v-if="show_job">
                             <job :job="job"></job>
+                        </div>
+                        <div v-if="show_container">
+                            <content-loader
+                                viewBox="0 0 350 600"
+                                :speed="2"
+                                primaryColor="#e1dbdb"
+                                secondaryColor="#ecebeb"
+                            >
+                                <rect x="10" y="56" rx="0" ry="0" width="139" height="36" /> 
+                                <rect x="10" y="12" rx="0" ry="0" width="271" height="26" /> 
+                                <rect x="10" y="128" rx="0" ry="0" width="271" height="19" /> 
+                                <rect x="10" y="169" rx="0" ry="0" width="156" height="18" /> 
+                                <rect x="10" y="203" rx="0" ry="0" width="271" height="19" /> 
+                                <rect x="10" y="246" rx="0" ry="0" width="271" height="19" /> 
+                                <rect x="10" y="286" rx="0" ry="0" width="271" height="19" /> 
+                                <rect x="10" y="340" rx="0" ry="0" width="139" height="36" />
+                            </content-loader>
                         </div>
                    </div>
                 </div>
@@ -101,6 +131,7 @@
 </template>
 
 <script>
+import { ContentLoader } from 'vue-content-loader'
 
 export default {
     data() {
@@ -112,8 +143,13 @@ export default {
             query: '',
             selectedCategories: [],
             selectedIndustries: [],
-            show_job: false
+            show_job: false,
+            show_container: false,
+            show_job_container: true
         }
+    },
+    components: {
+        ContentLoader
     },
     created() {
         this.getJobs()
@@ -124,6 +160,7 @@ export default {
         getJobs() {
             axios.get('api/jobs')
             .then(res => {
+                this.show_job_container = false;
                 this.jobs = res.data.data
             }).catch(err => console.log(err)) 
         },
@@ -140,6 +177,7 @@ export default {
             }).catch(err => console.log(err)) 
         },
         searchJobs() {
+            this.show_job_container = true;
             let arrayCategories = [];
             let arrayIndustries = [];
             this.selectedCategories.forEach(category => arrayCategories.push(category))
@@ -154,15 +192,18 @@ export default {
 
             axios.request(options).then((response) => {
                 this.jobs = response.data.data
+                this.show_job_container = false;
             }).catch(function (error) {
                 console.error(error)
             });
         },
         showJob(id) {
+            this.show_container = true;
             this.show_job = false
             axios.get('api/job/'+id)
             .then(res => {
                 this.job = res.data.data
+                this.show_container = false
                 this.show_job = true
             }).catch(err => console.log(err)) 
         }
